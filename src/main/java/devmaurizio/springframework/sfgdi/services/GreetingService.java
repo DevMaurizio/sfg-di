65c1a4aeb433a22693f8cfdfb02ae6aca65fcdfe
L0elz <maurizio.vdwerf@gmail.com>
2021-03-05 17:59:06 +0100

    fin01

---
+++ src/main/java/devmaurizio/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,6 @@
+package devmaurizio.springframework.sfgdi.services;
+
+public interface GreetingService {
+
+    String sayGreeting();
+}
---
+++ src/main/java/devmaurizio/springframework/sfgdi/services/PropertyInjectedGreetingService.java
@@ -0,0 +1,11 @@
+package devmaurizio.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropertyInjectedGreetingService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hello World - Property";
+    }
+}
